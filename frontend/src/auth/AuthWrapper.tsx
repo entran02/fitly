d982fdd9835d01d86b061b8930a52f9fa19caac4
Aidan Matrician <aidanmatrician@gmail.com>
2024-04-17 23:21:48 -0400

    fix

--- frontend/src/auth/AuthWrapper.tsx
+++ frontend/src/auth/AuthWrapper.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState } from "react";
 import RenderHeader from "../components/structure/Header.tsx";
 import RenderRoutes from "../components/structure/RenderRoutes.tsx";
 import RenderMenu from "../components/structure/RenderMenu.tsx";
@@ -6,91 +6,93 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
-const AuthContext = createContext({user: {name: '', isAuthenticated: false, id: ''}, login: (username, password) => {}, logout: () => {}, createAccount: (username: string, password1: string, password2: string) => {}});
+const AuthContext = createContext({
+  user: { name: "", isAuthenticated: false, id: "" },
+  login: (username, password) => {},
+  logout: () => {},
+  createAccount: (username: string, password1: string, password2: string) => {},
+});
 export const AuthData = () => useContext(AuthContext);
 
-
 export const AuthWrapper = () => {
+  const [user, setUser] = useState({
+    name: "",
+    isAuthenticated: false,
+    id: "",
+  });
 
-     const [ user, setUser ] = useState({name: "", isAuthenticated: false, id: ''})
+  const login = async (userName, password) => {
+    try {
+      const response = await axios.post("http://localhost:8000/api/login", {
+        username: userName,
+        password: password,
+      });
+      const data = response.data;
+      console.log("DATA: " + data);
+      // Handle the response data
 
-     const login = async (userName, password) => {
+      return new Promise((resolve, reject) => {
+        if (data.username === userName) {
+          console.log("USER ID =" + data.id);
+          console.log("USERNAME =" + data.username);
+          setUser({ name: userName, isAuthenticated: true, id: data.id });
+          resolve("success");
+        } else {
+          reject("Incorrect password");
+        }
+      });
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle any errors
+      return new Promise((resolve, reject) => {
+        reject("Incorrect password");
+      });
+    }
 
-          try {
-               const response = await axios.post('http://localhost:8000/api/login', { username: userName, password: password });
-               const data = response.data;
-               console.log('DATA: ' + data)
-               // Handle the response data
+    // Make a call to the authentication API to check the username
+  };
 
-               return new Promise((resolve, reject) => {
+  const createAccount = async (userName, password1, password2) => {
+    if (password1 !== password2) {
+      return new Promise((resolve, reject) => {
+        reject("Passwords do not match");
+      });
+    }
 
-                    if (data.username === userName) {
-                         console.log('USER ID =' + data.id)
-                         console.log('USERNAME =' + data.username)
-                         setUser({name: userName, isAuthenticated: true, id: data.id});
-                         resolve("success");
-                    } else {
-                         reject("Incorrect password");
-                    }
-               })
-          } catch (error) {
-               console.error('Error:', error);
-               // Handle any errors
-               return new Promise((resolve, reject) => {
-                    reject("Incorrect password");
-               })
-          }
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/create/user",
+        { username: userName, password: password1 }
+      );
+      const data = response.data;
+      const errMsg = response.statusText;
 
-          // Make a call to the authentication API to check the username
-          
-          
-     }
+      await new Promise((resolve, reject) => {
+        setUser({ name: userName, id: data.id, isAuthenticated: true });
+        resolve("success");
+      });
+      return login(userName, password1);
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle any errors
+      return new Promise((resolve, reject) => {
+        reject("Try a different username");
+      });
+    }
 
-     const createAccount = async (userName, password1, password2) => {
-          if (password1 !== password2) {
-               return new Promise((resolve, reject) => {
-                    reject("Passwords do not match")
-               })
-          }
+    // Make a call to the authentication API to check the username
+  };
 
-          try {
-               const response = await axios.post('http://localhost:8000/api/create/user', { username: userName, password: password1 });
-               const data = response.data;
-               const errMsg = response.statusText;
+  const logout = () => {
+    setUser({ name: "", isAuthenticated: false, id: "" });
+  };
 
-               return new Promise((resolve, reject) => {
-
-                    setUser({name: userName , id: data.id, isAuthenticated: true});
-                    resolve("success");
-               })
-          } catch (error) {
-               console.error('Error:', error);
-               // Handle any errors
-               return new Promise((resolve, reject) => {
-                    reject("Try a different username");
-               })
-          }
-
-          // Make a call to the authentication API to check the username
-          
-          
-     }
-
-     const logout = () => {
-
-          setUser({name: '', isAuthenticated: false, id: ''})
-     }
-
-
-     return (
-          
-               <AuthContext.Provider value={{user, login, logout, createAccount}}>
-                    <>
-                         <RenderMenu />
-                         <RenderRoutes />
-                    </>
-               </AuthContext.Provider>
-          
-     )
-
-}
+  return (
+    <AuthContext.Provider value={{ user, login, logout, createAccount }}>
+      <>
+        <RenderMenu />
+        <RenderRoutes />
+      </>
+    </AuthContext.Provider>
+  );
+};
